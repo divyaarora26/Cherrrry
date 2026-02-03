7fd1b11f135326bb7cd6d5209c9980761287a5a2
divyaarora26 <divyaarora6543@gmail.com>
2026-02-03 23:07:47 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,64 @@
+let currentScreen = 0;
+const screens = document.querySelectorAll(".screen");
+
+const bgMusic = document.getElementById("bgMusic");
+const heartBeat = document.getElementById("heartBeat");
+
+const typedText = document.getElementById("typedText");
+
+const LETTER = [
+  "Dear Cherry 🍒",
+  "",
+  "I love you so much more than I could ever fully explain and being with you is one of the greatest gifts in my life.",
+  "You mean everything to me.",
+  "You are incredibly beautiful and absolutely gorgeous inside and out.",
+  "Love your smile, your eyes and the whole you baby.",
+  "",
+  "Lucky to call you mine.",
+  "I love you, always and forever ♾️",
+  "",
+  "Your Daddy"
+];
+
+let line = 0;
+let char = 0;
+let typingStarted = false;
+
+/* NEXT BUTTON */
+function nextScreen() {
+  screens[currentScreen].classList.remove("active");
+  currentScreen++;
+
+  if (currentScreen < screens.length) {
+    screens[currentScreen].classList.add("active");
+  }
+
+  /* Start music once */
+  if (currentScreen === 1) {
+    bgMusic.play();
+    heartBeat.play();
+  }
+
+  /* Start typing letter */
+  if (screens[currentScreen].id === "letter" && !typingStarted) {
+    typingStarted = true;
+    typeLetter();
+  }
+}
+
+/* TYPEWRITER */
+function typeLetter() {
+  const typing = setInterval(() => {
+    if (line < LETTER.length) {
+      if (char < LETTER[line].length) {
+        typedText.innerHTML += LETTER[line][char++];
+      } else {
+        typedText.innerHTML += "<br><br>";
+        line++;
+        char = 0;
+      }
+    } else {
+      clearInterval(typing);
+    }
+  }, 35);
+}
